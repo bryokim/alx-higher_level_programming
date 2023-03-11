aa51a99286e7bfd8ca8c64ad05bcea587bfb01b6
Brian Kim <bryo.kim@gmail.com>
2023-03-11 22:10:31 +0300

    Added format specifier

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,8 +5,8 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
 
-    max = 0
-    for num in my_list:
+    max = my_list[0]
+    for num in my_list[1:]:
         if num > max:
             max = num
     return max
