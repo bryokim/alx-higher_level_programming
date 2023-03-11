aeefb35f0bc7b107e3de64dff45d54241bd5f857
Brian Kim <bryo.kim@gmail.com>
2023-03-11 23:05:59 +0300

    Added object.h and listobject.h header files

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,29 +1,9 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <object.h>
+#include <listobject.h>
 #include <stdio.h>
 
-/**
- * print_type_str - print the type of the item
- *
- * @item: item to get type
- */
-void print_type_str(PyObject *item)
-{
-	char *str;
-
-	if (PyFloat_Check(item))
-		str = "float\n";
-	else if (PyLong_Check(item))
-		str = "int\n";
-	else if (PyList_Check(item))
-		str = "list\n";
-	else if (PyTuple_Check(item))
-		str = "tuple\n";
-	else
-		str = "str\n";
-
-	printf("%s", str);
-}
 
 /**
  * print_python_list_info - print info about a Python list.
@@ -34,20 +14,21 @@ void print_python_list_info(PyObject *p)
 {
 	Py_ssize_t idx, len;
 	PyObject *item;
+	PyListObject *obj;
 
 	/* If p is not a list or an instance of a list exit*/
 	if (!PyList_Check(p))
 		return;
 
+	obj = (PyListObject *)p;
 	len = PyList_Size(p);
+
 	fprintf(stdout, "[*] Size of the Python list = %ld\n", len);
-	fprintf(stdout, "[*] Allocated = \n");
+	fprintf(stdout, "[*] Allocated = %ld\n", obj->allocated);
 
 	for (idx = 0; idx < len; idx++)
 	{
 		item = PyList_GetItem(p, idx);
-
-		fprintf(stdout, "Element %ld: ", idx);
-		print_type_str(item);
+		fprintf(stdout, "Element %ld: %s\n", idx, Py_TYPE(item)->tp_name);
 	}
 }
