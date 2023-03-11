bf8257d0881806adc1e64462414638887d83731b
Brian Kim <bryo.kim@gmail.com>
2023-03-11 22:12:33 +0300

    Changed for loop statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for i in reversed(my_list):
-        print("{:d}".format(i))
+    upper_range = -(len(my_list) + 1)
+    for i in range(-1, upper_range, -1):
+        print("{:d}".format(my_list[i]))
