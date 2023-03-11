04ad5b09355c4c7f724d8e3cb07140ab87060527
Brian Kim <bryo.kim@gmail.com>
2023-03-11 23:27:23 +0300

    Removed check for list argument

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,8 +1,6 @@
-#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <object.h>
 #include <listobject.h>
-#include <stdio.h>
 
 
 /**
@@ -16,19 +14,15 @@ void print_python_list_info(PyObject *p)
 	PyObject *item;
 	PyListObject *obj;
 
-	/* If p is not a list or an instance of a list exit*/
-	if (!PyList_Check(p))
-		return;
-
 	obj = (PyListObject *)p;
 	len = PyList_Size(p);
 
-	fprintf(stdout, "[*] Size of the Python list = %ld\n", len);
-	fprintf(stdout, "[*] Allocated = %ld\n", obj->allocated);
+	printf("[*] Size of the Python list = %ld\n", len);
+	printf("[*] Allocated = %ld\n", obj->allocated);
 
 	for (idx = 0; idx < len; idx++)
 	{
 		item = PyList_GetItem(p, idx);
-		fprintf(stdout, "Element %ld: %s\n", idx, Py_TYPE(item)->tp_name);
+		printf("Element %ld: %s\n", idx, Py_TYPE(item)->tp_name);
 	}
 }
