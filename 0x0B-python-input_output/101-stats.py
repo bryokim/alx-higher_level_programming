53e14ce17cbed79cac6327acda6741fe41ce69f3
Brian Kimathi <bryo.kim1@gmail.com>
2023-04-13 15:07:15 +0300

    Added handling of different line counts and error handling.
    The module can be able to process any number of lines in file
    and print the metrics accordingly. It can also handle errors
    arising from wrong formats and exits while printing metrics
    calculated before the error occured.

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-"""Module that computes metrics"""
+"""
+Module that reads stdin and computes metrics and prints to stdout.
+
+Input format:
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+
+Each 10 lines and after a keyboard interruption (CTRL-C) prints the statistics
+since the beginning. See print_statistics function for printing format.
+"""
 
 import sys
 
@@ -16,7 +24,7 @@ status_codes = {
 }
 
 
-def print_details(size, codes):
+def print_statistics(size, codes):
     """Prints details about metrics.
 
     Args:
@@ -26,33 +34,64 @@ def print_details(size, codes):
     """
     print(f"File size: {size}")
     for key, value in codes.items():
-        if value:
+        if value:   # Print <status code>: <number> if number is not 0.
             print(f"{key}: {value}")
 
 
-line = sys.stdin.readline()
-counter = 0
-try:
-    while line:
-        size = ""
-        code = ""
-        i = 0
-        for char in reversed(line):
-            if char == ' ':
-                i += 1
-            elif not i:
-                size += char
-            elif i:
-                code += char
+def read_size_and_code(line: str):
+    """Read file size and status code from line.
 
-            if i == 2:
-                break
-        file_size += int(size[1:])
-        status_codes[code[-1::-1]] += 1
-        line = sys.stdin.readline()
-        counter += 1
+    Args:
+        line (str): Line of text as red from stdin.
 
-        if counter % 10 == 0:
-            print_details(file_size, status_codes)
-except KeyboardInterrupt:
-    print_details(file_size, status_codes)
+    Returns:
+        tuple: Tuple of both size and code.
+    """
+    size = ""
+    code = ""
+    i = 0
+
+    for char in reversed(line):
+        if char == ' ':
+            i += 1
+        elif not i:
+            size += char
+        elif i:
+            code += char
+
+        if i == 2:
+            break
+    if size[0] == '\n':
+        size = size[1:]
+    return (size[-1::-1], code[-1::-1])
+
+
+def compute_metrics():
+    global file_size
+    global status_codes
+    line = sys.stdin.readline()
+    counter = 0
+    try:
+        while line:
+            size, code = read_size_and_code(line)
+
+            file_size += int(size)
+            status_codes[code] += 1
+
+            counter += 1
+            line = sys.stdin.readline()
+            while line == '\n':  # Skip blank lines.
+                line = sys.stdin.readline()
+
+            if counter == 10 or not line:  # Print if counter is 10 or if EOF.
+                print_statistics(file_size, status_codes)
+                counter = 0
+    except Exception as e:
+        print_statistics(file_size, status_codes)
+        sys.exit(f"[{e.__class__.__name__}]: {e}")
+    except KeyboardInterrupt:
+        print_statistics(file_size, status_codes)
+
+
+if __name__ == "__main__":
+    compute_metrics()
