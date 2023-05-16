2cd9352db65b6a84d84489c78d3e4ddc256aa8e7
Brian Kimathi <bryo.kim1@gmail.com>
2023-05-16 20:20:33 +0300

    Removed importation of temperatures.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- Converts the first_table to UTF8
 USE hbtn_0c_0;
-ALTER TABLE first_table CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,4 @@
 -- Displays the average temperature(Fahrenheit) by city ordered by
 -- temperature(descending).
 USE hbtn_0c_0
-SET autocommit=0; source temperatures.sql; COMMIT;
 SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
