6d2a3f1f9c7cc93523496d89829933ad2ffe3ec3
Brian Kimathi <bryo.kim1@gmail.com>
2023-05-16 20:16:49 +0300

    Removed importation of temperatures.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,5 @@
 -- Displays the max temperature of each state(ordered by State name)
 USE hbtn_0c_0
-SET autocommit=0; source temperatures.sql; COMMIT;
 SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
