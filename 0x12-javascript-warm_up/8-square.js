898f66d0cbb177841d5c568e8beaee9d5002e9d2
Brian Kim <bryo.kim1@gmail.com>
2023-06-12 17:28:26 +0300

    Changed display character to X

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,6 +6,6 @@ if (Number.isNaN(size)) {
   console.log('Missing size');
 } else {
   let side = '';
-  for (let i = 0; i < size; side += 'x', i++);
+  for (let i = 0; i < size; side += 'X', i++);
   for (let i = 0; i < size; i++) console.log(side);
 }
