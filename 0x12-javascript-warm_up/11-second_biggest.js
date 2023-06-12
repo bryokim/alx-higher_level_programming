4818249cf2a6a39f9c07e0aea13e119089d07d03
Brian Kim <bryo.kim1@gmail.com>
2023-06-12 17:26:59 +0300

    Enabled working for an array of same number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -12,11 +12,11 @@ function secondLargest (arr) {
       second = item;
     }
   });
-  return second;
+  return (arr.length === 2 && second === 0) ? arr[1] : second;
 }
 
 if (process.argv.length === 2 || process.argv.length === 3) {
-  console.log(0);
+  console.log('0');
 } else {
-  console.log(secondLargest(process.argv.slice(2)));
+  console.log(secondLargest(process.argv.slice(2).map(Number)));
 }
