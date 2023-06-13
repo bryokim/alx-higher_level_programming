13324ffba069b721a1c023623ecc771f0b393f51
Brian Kim <bryo.kim1@gmail.com>
2023-06-13 16:38:11 +0300

    Formated to semistandard specs

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,7 +6,6 @@ const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-
 if (fileA && fileB && fileC) {
   [fileA, fileB].forEach(file => {
     readFile(file, 'utf8', (error, data) => {
