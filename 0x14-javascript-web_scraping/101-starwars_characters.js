fb1aabf7c3b617093bacf5aac426488c0c19288f
Brian Kimathi <bryo.kim1@gmail.com>
2023-07-25 20:42:47 +0300

    Waits on a request to end before starting another

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,19 +4,23 @@ const request = require('request');
 
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-request(url, (err, res, body) => {
+function getCharacterName (character) {
+  return new Promise((resolve) => {
+    request(character, function (err, res, body) {
+      if (err) console.log(err);
+      if (body) resolve(JSON.parse(body).name);
+    });
+  });
+}
+
+request(url, async (err, res, body) => {
   if (err) console.log(err);
   if (body) {
     const characters = JSON.parse(body).characters;
 
     for (const character of characters) {
-      request(character, (err, res, body) => {
-        if (err) console.log(err);
-        if (body) {
-          const characterName = JSON.parse(body).name;
-          console.log(characterName);
-        }
-      });
+      const name = await getCharacterName(character);
+      console.log(name);
     }
   }
 });
